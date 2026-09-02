06bb35a0a70155b30c031be03e63212598394dd9
ImVeenz <187037459+ImVeenz@users.noreply.github.com>
2026-09-02 03:31:52 +0200

    commit for exercise 1.2

--- Part1/Courseinfo/src/App.jsx
+++ Part1/Courseinfo/src/App.jsx
@@ -7,15 +7,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+
+  return(
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
 const Content = (props) => {
   return (
-    <>
-      <p>{props.part1}  {props.exercises1}</p>
-      <p>{props.part2}  {props.exercises2}</p>
-      <p>{props.part3}  {props.exercises3}</p>
-      
-      
-    </>
+    <div>
+      <Part part={props.part1} exercise={props.exercises1}></Part>
+      <Part part={props.part2} exercise={props.exercises2}></Part>
+      <Part part={props.part3} exercise={props.exercises3}></Part>
+    </div>
     
   )
 }
@@ -44,9 +49,7 @@ const App = () => {
         part1={part1} exercises1={exercises1}
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3}
-      >
-
-      </Content>
+      />
       <Total sum={exercises1 + exercises2 + exercises3}></Total>
     </>
   )
