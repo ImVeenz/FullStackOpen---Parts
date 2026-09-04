80527f5de226fccffe1bcd230e22b1bb6ded091b
Veenz <187037459+ImVeenz@users.noreply.github.com>
2026-09-04 03:25:19 +0200

    1.7 unicafe step 2

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -14,11 +14,20 @@ const Button=({onClick,text})=>{
 }
 
 const Statistics = ({good,neutral,bad}) =>{
+
+
+  const total = good + neutral + bad
+  const average = (good-bad)/total
+  const positive = (good/total)*100
+
   return(
     <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
 
     </>
   )
