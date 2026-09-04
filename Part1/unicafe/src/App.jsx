d2306d08bf7293af187d6effcc06510ba8c26a10
Veenz <187037459+ImVeenz@users.noreply.github.com>
2026-09-04 04:13:50 +0200

    1.10 unicafe step 5

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -13,6 +13,12 @@ const Button=({onClick,text})=>{
   )
 }
 
+const StatisticLine = ({text,value}) => {
+  return(
+    <p>{text} {value}</p>
+  )
+}
+
 const Statistics = ({good,neutral,bad}) =>{
   const total = good + neutral + bad
   const average = (good-bad)/total
@@ -27,13 +33,13 @@ const Statistics = ({good,neutral,bad}) =>{
   
 
   return(
-    <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+    <>    
+      <StatisticLine text = "good" value={good}/>
+      <StatisticLine text = "neutral" value={neutral}/>
+      <StatisticLine text = "bad" value={bad}/>
+      <StatisticLine text = "all" value={total}/>
+      <StatisticLine text = "average" value={average}/>
+      <StatisticLine text = "positive" value={`${positive} %`}/>  
     </>
   )
 }
